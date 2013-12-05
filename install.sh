583ceefa0813e39f96b17f40222dd90a969e2574
Adam Lazzarato <adam.lazzarato@gmail.com>
2013-12-05 14:50:47 -0500

    Remove old installations

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # add executability permission to rsink.sh
 chmod +x .rsink/rsink.sh
 
+# remove current installation (if it exists)
+rm -fr ~/.rsink
+
 # move .rsink to home directory
 mv .rsink/ ~
 
